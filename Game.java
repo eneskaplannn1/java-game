87014c29c3ae77c49d9ba2a6fd1f0bd1a7ddf14f
Enes Kaplan <muhammedenes.kaplan@std.yeditepe.edu.tr>
2024-02-12 23:01:14 +0300

    implemented store

--- Game.java
+++ Game.java
@@ -23,12 +23,12 @@ public class Game {
       System.out.println("");
       System.out.println("================");
       System.out.println();
-      System.out.print("Choose a location. ");
       System.out.println("1-Home --> Safe place , No enemy");
       System.out.println("2-Cave --> You will encounter with zombies ");
       System.out.println("3-Forest --> You will encounter with vampire ");
       System.out.println("4-River --> You will encounter with bear ");
-      System.out.println("4-Store --> You can buy armour and weapon ");
+      System.out.println("5-Store --> You can buy armour and weapon ");
+      System.out.print("Choose location: ");
 
       int locChoice = input.nextInt();
 
@@ -37,15 +37,18 @@ public class Game {
         System.out.println("2-Cave --> You will encounter with zombies ");
         System.out.println("3-Forest --> You will encounter with vampire ");
         System.out.println("4-River --> You will encounter with bear ");
-        System.out.println("4-Store --> You can buy armour and weapon ");
+        System.out.println("5-Store --> You can buy armour and weapon ");
         System.out.print("Choose valid location: ");
         locChoice = input.nextInt();
       }
 
+      System.out.println("\n \n \n");
       switch (locChoice) {
         case 1:
           location = new House(player);
           break;
+        case 5:
+          location = new Store(player);
       }
 
       location.getLocation();
--- Store.java
+++ Store.java
@@ -1,1 +1,77 @@
-public class Store {}
+import javax.management.loading.PrivateClassLoader;
+
+public class Store extends SafeLocation {
+
+  Store(Player player) {
+    super(player, "Store");
+  }
+
+  @Override
+  public boolean getLocation() {
+    System.err.println("Players money = " + player.getMoney());
+    System.out.println("1. Weapons");
+    System.out.println("2. Armours");
+    System.out.println("3. Exit");
+    System.out.print("Your choice: ");
+    int choice = scan.nextInt();
+    int itemId;
+
+    switch (choice) {
+      case 1:
+        itemId = weaponMenu();
+        buyWeapon(itemId);
+      //   case 2:
+      //     itemId = armourMenu();
+      //     buyArmour(itemId);
+      //   case 3:
+    }
+
+    return true;
+  }
+
+  public int weaponMenu() {
+    System.out.println("1. Gun <Cost : 25 - Damage - 2>");
+    System.out.println("2. Sword <Cost : 35 - Damage - 3>");
+    System.out.println("3. Rifle <Cost : 45 - Damage - 7>");
+    System.out.println("4. Exit");
+    System.err.print("Enter your choice: ");
+
+    return scan.nextInt();
+  }
+
+  public void buyWeapon(int weaponId) {
+    int damage = 0, price = 0;
+    String wName = null;
+    switch (weaponId) {
+      case 1:
+        damage = 2;
+        wName = "Gun";
+        price = 25;
+      case 2:
+        damage = 4;
+        wName = "Sword";
+        price = 35;
+      case 3:
+        damage = 7;
+        wName = "Rifle";
+        price = 45;
+      default:
+        System.out.println("Invalid action");
+    }
+    if (player.getMoney() > price) {
+      player.getInv().setDamage(damage);
+      player.getInv().setWeaponName(wName);
+      player.setMoney(player.getMoney() - price);
+      System.out.println(
+        "You buyed " +
+        wName +
+        " previous damage: " +
+        player.getDamage() +
+        " current damage: " +
+        (player.getDamage() + player.getInv().getDamage())
+      );
+    } else {
+      System.out.println("You dont have enough credit");
+    }
+  }
+}
